62748b8760ec454b7c7410b7f72b2d754ff188ec
Hannah Danziger <hdanziger8@gmail.com>
2017-10-04 14:39:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,11 +9,18 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
-  # write your code here
-  html = File.read('fixtures/kickstarter.html')
+   # write your code here
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
 
-kickstarter = Nokogiri::HTML(html)
-binding.pry
+    projects = {}
+
+    kickstarter.css("li.project.grid_4").each do |project|
+      projects[project] = {}
+    end
+
+    projects
+
 end
 
 create_project_hash
